1468eb5214a8c5d8e01acf5c52054035f486c4e7
Anton <anton.troyan7@gmail.com>
2023-07-14 23:00:57 +0200

    Example works

--- components/ui/main.tsx
+++ components/ui/main.tsx
@@ -3,19 +3,19 @@ import { Separator } from "@/components/ui/separator"
 
 export default function Main() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-52">
       <div>
         <div className="space-y-1">
-          <h4 className="text-2xl font-medium leading-none">Radix Primitives</h4>
+          <h4 className="text-2xl font-medium leading-none">Thin, flexible string or rope made from several twisted strands</h4>
           <p className="text-2xl text-muted-foreground">
-            An open-source UI component library.
+            Hang the picture from a rail on a length of [...].
           </p>
         </div>
         <Separator className="my-4" />
         <div className="flex h-5 items-center space-x-4 text-2xl">
-          <div>Blog</div>
+          <div>Cord</div>
           <Separator orientation="vertical" />
-          <div>Docs</div>
+          <div>Feather</div>
           <Separator orientation="vertical" />
           <div>Source</div>
         </div>
