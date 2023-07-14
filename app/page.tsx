7623c6a41ea384853ab9ddc00628a2b48a8bea9b
Anton <anton.troyan7@gmail.com>
2023-07-14 22:14:34 +0200

    Dark theme works

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
 import Image from 'next/image'
 import { Separator } from "@/components/ui/separator"
+import Main from '@/components/ui/main'
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <div className="space-y-1">
-          <h4 className="text-sm font-medium leading-none">Radix Primitives</h4>
-          <p className="text-sm text-muted-foreground">
-            An open-source UI component library.
-          </p>
-        </div>
-        <Separator className="my-4" />
-        <div className="flex h-5 items-center space-x-4 text-sm">
-          <div>Blog</div>
-          <Separator orientation="vertical" />
-          <div>Docs</div>
-          <Separator orientation="vertical" />
-          <div>Source</div>
-        </div>
-      </div>
-    </main>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <Main/>
+    </ThemeProvider>
   )
 }
