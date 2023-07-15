db96f362352b8b0baf363018e5a14bcd294ad6a3
Anton <anton.troyan7@gmail.com>
2023-07-15 17:17:37 +0200

    UseState almost work

--- components/ui/main.tsx
+++ components/ui/main.tsx
@@ -7,7 +7,7 @@ import { entity } from '../types';
 
 export default function Game() {
 
-    const currentData = {
+    const [currentData, setCurrentData] = useState({
         correct: {
             meaning: "",
             example: "",
@@ -16,40 +16,56 @@ export default function Game() {
         wrong: [
             "", "", "", ""
         ]
-    }
-
-
-    // HOOKS
-    useEffect(() => {
-        fillCurrentData(Data);
-    }, []);
+    });
 
     const { toast } = useToast()
-    const [count, setCount] = React.useState(0);
 
     const getRandom = (array: Array<entity>): entity => {
         return array[Math.floor(Math.random() * array.length)]
     }
 
     const fillCurrentData = (array: Array<entity>) => {
+        debugger
         const randomCorrect = getRandom(array)
+        const randomWrongResult:string[] = []
 
-        currentData.correct = randomCorrect
         let wrongElementsCounter = 0
         while (wrongElementsCounter < 3) {
             let randomWrong = getRandom(array)
             if (randomWrong !== randomCorrect // add element only if it doesn match correct and was not added earlier
-                    && !currentData.wrong.some(element => element === randomWrong.word)) { 
+                && !currentData.wrong.some(element => element === randomWrong.word)) {
 
-                currentData.wrong[wrongElementsCounter] = randomWrong.word
+                randomWrongResult[wrongElementsCounter] = randomWrong.word
                 wrongElementsCounter++
             }
         }
-        console.log(currentData)
+        setCurrentData({...currentData, correct: randomCorrect, wrong: randomWrongResult})
     }
 
     const handleNext = () => {
 
+    }
+
+    const createVariants = (): React.JSX.Element[] => {
+        if (currentData.correct.word == "") {
+            debugger
+            fillCurrentData(Data);
+        }
+
+        return currentData.wrong.map(element => {
+            return (
+                <div key={element}>
+                    <div onClick={() => {
+                        toast({
+                            variant: "destructive",
+                            title: "Thin, flexible string or rope made from several twisted strands",
+                            description: "Cord",
+                        })
+                    }}>{element}</div>
+                    <Separator orientation="vertical" />
+                </div>
+            )
+        })
     }
 
     return (
@@ -63,21 +79,7 @@ export default function Game() {
                 </div>
                 <Separator className="my-4" />
                 <div className="flex h-5 items-center space-x-4 text-2xl">
-                    <div onClick={() => {
-                        toast({
-                            variant: "destructive",
-                            title: "Thin, flexible string or rope made from several twisted strands",
-                            description: "Cord",
-                        })
-                    }}>Cord</div>
-                    <Separator orientation="vertical" />
-                    <div>Feather</div>
-                    <Separator orientation="vertical" />
-                    <div>Source</div>
-                    <Separator orientation="vertical" />
-                    <div>Test</div>
-                    <Separator orientation="vertical" />
-                    <div>Java</div>
+                    {createVariants()}
                 </div>
             </div>
         </main>
