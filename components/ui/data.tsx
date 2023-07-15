b6eea899fd5e956bdb1c7a1bdd4f45b80472107b
Anton <anton.troyan7@gmail.com>
2023-07-15 21:09:10 +0200

    Add new words

--- components/ui/data.tsx
+++ components/ui/data.tsx
@@ -25,7 +25,67 @@ const Data: entity[] =
             example: "The bite [...]ed like crazy",
             word: "Itch",
             tags: ["#harry_potter", "#films"]
-        }
+        },
+        {
+            meaning: "To attract, entice, or tempt; Allure",
+            example: "We've got the trap set up, but we need some kind of bait to [...] the rabbits [...].",
+            word: "Lure in",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Shiny and smooth; Superficially attractive, stylish, and suggesting wealth",
+            example: "Thick, [...], manageable hair; A [...] TV miniseries",
+            word: "Glossy",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Someone who is very serious about their health",
+            example: "My aunt is a [...]. She runs six miles every morning before work.",
+            word: "Health nut",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "A routine task, especially a household one",
+            example: "The early risers were up and about, doing their [...]",
+            word: "Chore",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Infatuated or obsessed.",
+            example: "Gus is still hopelessly [...] by Lee",
+            word: "Twitterpated",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "(of a horse) of pure breed, especially of a breed originating from English mares and Arab stallions and widely used as racehorses",
+            example: "Some riders will only buy [...] horses",
+            word: "Thoroughbred",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Extreme tiredness resulting from mental or physical exertion or illness; ",
+            example: "He was nearly dead with [...]",
+            word: "Fatigue",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Not easily bent or changed in shape; rigid; Severe or strong.",
+            example: "A [...] black collar; They face [...] fines and a possible jail sentence",
+            word: "Stiff",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Comfortable, warm, and cosy; well protected from the weather or cold",
+            example: "She was safe and [...] in Ruth's arms",
+            word: "Snug",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
+        {
+            meaning: "Crouch or sit with one's knees bent and one's heels close to or touching one's buttocks or the back of one's thighs.",
+            example: "I [...]ed down in front of him",
+            word: "Squat",
+            tags: ["#how_heavy_are_the_dumbbells_you_lift?", "#anime"]
+        },
     ]
 
 
