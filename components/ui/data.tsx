666d0b45cd78ff5e10992afd3872599a6141ee5b
Anton <anton.troyan7@gmail.com>
2023-07-22 22:38:45 +0200

    Experiment

--- components/ui/data.tsx
+++ components/ui/data.tsx
@@ -135,48 +135,7 @@ const rawData: rawEntity[] =
             word: "Fickle",
             tags: ["shiki"]
         },
-        {
-            meaning: "",
-            example: "",
-            word: "Barren",
-            tags: [""]
-        },
-        {
-            meaning: "",
-            example: "",
-            word: "Trod",
-            tags: [""]
-        },
-        {
-            meaning: "",
-            example: "",
-            word: "",
-            tags: [""]
-        },
-        {
-            meaning: "",
-            example: "",
-            word: "",
-            tags: [""]
-        },
-        {
-            meaning: "",
-            example: "",
-            word: "",
-            tags: [""]
-        },
-        {
-            meaning: "",
-            example: "",
-            word: "",
-            tags: [""]
-        },
-        {
-            meaning: "",
-            example: "",
-            word: "",
-            tags: [""]
-        },                                                
+                               
     ]
 
 
